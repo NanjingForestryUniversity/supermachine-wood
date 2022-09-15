d0df5d42d334c9fa77493236be7ce901eeed8c41
FEIJINTI <83849113+FEIJINTI@users.noreply.github.com>
2022-09-15 14:14:05 +0800

    合并
    第一次合并

---
+++ socket_detector.py
@@ -0,0 +1,71 @@
+import socket
+import numpy as np
+import cv2
+from classifer import WoodClass
+import time
+
+
+def main():
+    model_path = "models/model_2022-09-06_13-08.p"
+    socket_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_receive.connect(('127.0.0.1', 21122))
+    socket_send = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_send.connect(('127.0.0.1', 21123))
+    # _ = socket_receive.recv(4096*1000*10)
+    detector = WoodClass(w=4096, h=1200, n=3000, debug_mode=False)
+    detector.load(path=model_path)
+    while True:
+        # receive data
+        t1 = time.time()
+        size_buff = socket_receive.recv(4)
+        n_rows, n_cols = size_buff[0] << 8 | size_buff[1], size_buff[2] << 8 | size_buff[3]
+        data_size = n_rows * n_cols * 3
+        print(data_size)
+        recv_size = data_size
+        buff_all, size = [], 0
+        while True:
+            data_buff = socket_receive.recv(recv_size)
+            recv_size -= len(data_buff)
+            buff_all += data_buff
+            if recv_size == 0:
+                break
+        print(len(buff_all))
+        raw_data = np.frombuffer(bytes(buff_all), dtype=np.uint8).reshape(int(n_rows), int(n_cols), -1)
+        print(raw_data.shape)
+        wood_color = detector.predict(raw_data)
+        # cv2.imshow("img", raw_data)
+        # cv2.waitKey(30)
+        # print('Class is ', wood_color)
+        if wood_color == 0:
+            socket_send.send(b'S')
+            print('S send success')
+        elif wood_color == 1:
+            socket_send.send(b'Z')
+            print('Z send success')
+        elif wood_color == 2:
+            socket_send.send(b'Q')
+            print('Q send success')
+        print((time.time()-t1))
+
+
+if __name__ == '__main__':
+    # 2个端口
+    # 接受端口21122
+    # 发送端口21123
+    # 接收到图片 n_rows * n_bands * n_cols, float32
+    # 发送图片 n_rows * n_cols, uint8
+    main()
+    # test(r"D:\build-tobacco-Desktop_Qt_5_9_0_MSVC2015_64bit-Release\calibrated15.raw")
+    # main()
+    # debug_main()
+    # test_run(all_data_dir=r'D:\数据')
+    # with open(r'D:\数据\虫子\valid2.raw', 'rb') as f:
+    #     data = np.frombuffer(f.read(), dtype=np.float32).reshape(600, 29, 1024).transpose(0, 2, 1)
+    # plt.matshow(data[:, :, 10])
+    # plt.show()
+    # detector = SpecDetector('model_spec/model_29.p')
+    # result = detector.predict(data)
+    #
+    # plt.matshow(result)
+    # plt.show()
+    # result = result.reshape((600, 1024))
---
+++ utils.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Nov 3 21:18:26 2020
+
+@author: l.z.y
+@e-mail: li.zhenye@qq.com
+"""
+import os
+import shutil
+import time
+
+
+def mkdir_if_not_exist(dir_name, is_delete=False):
+    """
+    创建文件夹
+    :param dir_name: 文件夹
+    :param is_delete: 是否删除
+    :return: 是否成功
+    """
+    try:
+        if is_delete:
+            if os.path.exists(dir_name):
+                shutil.rmtree(dir_name)
+                print('[Info] 文件夹 "%s" 存在, 删除文件夹.' % dir_name)
+
+        if not os.path.exists(dir_name):
+            os.makedirs(dir_name)
+            print('[Info] 文件夹 "%s" 不存在, 创建文件夹.' % dir_name)
+        return True
+    except Exception as e:
+        print('[Exception] %s' % e)
+        return False
+
+
+def create_file(file_name):
+    """
+    创建文件
+    :param file_name: 文件名
+    :return: None
+    """
+    if os.path.exists(file_name):
+        print("文件存在：%s" % file_name)
+        return False
+        # os.remove(file_name)  # 删除已有文件
+    if not os.path.exists(file_name):
+        print("文件不存在，创建文件：%s" % file_name)
+        open(file_name, 'a').close()
+        return True
+
+
+class Logger(object):
+    def __init__(self, is_to_file=False, path=None):
+        self.is_to_file = is_to_file
+        if path is None:
+            path = "wood.log"
+        self.path = path
+        create_file(path)
+
+    def log(self, content):
+        if self.is_to_file:
+            with open(self.path, "a") as f:
+                print(time.strftime("[%Y-%m-%d_%H-%M-%S]:"), file=f)
+                print(content, file=f)
+        else:
+            print(content)
+
+
+if __name__ == '__main__':
+    log = Logger(is_to_file=True)
+    log.log("nihao")
+    import numpy as np
+    a = np.ones((100, 100, 3))
+    log.log(a.shape)
